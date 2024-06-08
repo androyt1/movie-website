7e2d759e69dc8bbcb5932897ff35faa76ea050ec
Andrew Aghoghovwia <androyt1@gmail.com>
2024-06-08 07:58:46 +0100

    updated and moved files

--- src/BoatVideo.tsx
+++ src/BoatVideo.tsx
@@ -1,14 +1,13 @@
 function BoatVideo() {
-  return (
-    <video
-      className="h-100vh w-full object-cover animate-clip-from-top-animation"
-      autoPlay
-      muted
-      loop
-    >
-      <source src="src/assets/boat-video.mp4" type="video/mp4" />
-    </video>
-  );
+    return (
+        <video
+            className='h-100vh w-full object-cover animate-clip-from-top-animation'
+            autoPlay
+            muted
+            loop>
+            <source src='./assets/boat-video.mp4' type='video/mp4' />
+        </video>
+    );
 }
 
 export default BoatVideo;
